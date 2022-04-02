830c287f3afecb877ac9ea2c411fed989c1be6db
Lucas Bruck <lukabruck@gmail.com>
2022-04-02 12:24:45 +1100

    Use linear gradient bg

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -13,22 +13,15 @@ const welcomeTheme = {
 export default function Welcome() {
   return (
     <Grommet theme={welcomeTheme} data-testId="welcome">
-      <Box height="100vh" justify="center" background={{color: "dark-2"}}>
-        <Box
-          direction="row"
-          justify="center"
-          pad="xlarge"
-          background={{color: "dark-2"}}
-        >
-          <Card background={{color: "light-5"}} direction="column" justify="center" onClick={() => ({})}>
-            <CardBody pad="large">
-              <Box gap="small" align="center">
-                <Home size="large"/>
-                <Text color="dark-1">Welcome</Text>
-              </Box>
-            </CardBody>
-          </Card>
-        </Box>
+      <Box height="100vh" justify="center" align="center" background="linear-gradient(45deg, #ffb4c4, #003399)">
+        <Card background={{color: "light-5"}} direction="column" justify="center" onClick={() => ({})}>
+          <CardBody pad="large">
+            <Box gap="small" align="center">
+              <Home size="large"/>
+              <Text color="dark-1">Welcome</Text>
+            </Box>
+          </CardBody>
+        </Card>
       </Box>
     </Grommet>
   )
