51cb92d2f6e6160eea330487b4861388b0db4157
Lucas Bruck <lukabruck@gmail.com>
2024-05-07 20:33:44 +0100

    Little fixes here and there

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the entire app', () => {
-  render(<App />);
-  expect(screen.getByTestId("welcomeScreen")).toBeInTheDocument();
-});
--- src/components/Welcome.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Welcome from "./Welcome";
-
-test('renders the welcome component', () => {
-  render(<Welcome />);
-  expect(screen.getByText(/Lucas Bruck/i)).toBeInTheDocument();
-});
